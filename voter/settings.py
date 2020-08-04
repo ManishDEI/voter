300bddee37abe872203402002211ffbf706b102b
Manish <69155414+ManishDEI@users.noreply.github.com>
2020-08-04 20:01:32 +0530

    Update settings.py

--- voter/settings.py
+++ voter/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #Media Files
 MEDIA_URL = '/media/'
